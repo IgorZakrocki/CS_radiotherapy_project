2c08de87371117d768362d7e3c608a5e0417816f
Igor Zakrocki <igorzakrocki@pis-rr-unitek.iesx.krakow.pl>
2026-01-22 21:11:44 +0100

    Delete src/ folder

--- src/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Model definitions and simulation kernels."""
-
-from .pde import run_pde_simulation, Grid, SimConfig, run_2d_simulation
-from .ode import run_ode_simulation, physics_ode_derivative
-from .mlp import ResidualMLP, PINN_SuperNet, ODE_Supermodel, PINN, Supermodel
-from .node import run_node_simulation, run_supermodel_simulation
